9bb54f7d095c58412bec56cde287406115ce0e78
Psalmry <samuel.oladipo@tecteta.com>
2019-05-12 10:57:51 +0100

    Increased test cases tto 10

--- src/main/java/com/project/team2hackathon/controller/DefaultController.java
+++ src/main/java/com/project/team2hackathon/controller/DefaultController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.project.team2hackathon.InterestMatcher;
 import org.json.*;
 
 /**
@@ -27,41 +26,36 @@ public class DefaultController {
     
     @GetMapping(value = "getmatch/{id}/byrange/{range}", produces=MediaType.APPLICATION_JSON_VALUE)
     public Object matchById(@PathVariable("id") Integer id, @PathVariable("range") Integer range){
-        return interestMatchRun(id, range);
+        return runTest(id, range);
     }
     
-    public String interestMatchRun(int id, int needed){
-        ArrayList<Integer> userInterest = new ArrayList<>(); 
-           ArrayList<Integer> allInterest = new ArrayList<>(); 
-           ArrayList<Integer> allInterest2 = new ArrayList<>(); 
-           ArrayList<Integer> allInterest3 = new ArrayList<>(); 
+    public void runInterestDb(int id, int needed){
+
+    }
+	
+	public String runTest(int userId, int range){
+        ArrayList<Integer> userInterest = new ArrayList<>();
            Map<Integer, ArrayList<Integer>> all = new HashMap<>();
-           for(int i = 0; i < 97; i++){
-               userInterest.add(new Integer(i));
-               if(i >= 89){
-                   allInterest.add(new Integer(i));
+           for(int i = 1; i <= 100; i++){
+               ArrayList<Integer> interest = new ArrayList<>();
+               int rand = (int)(Math.random() * 100) + 1;
+               for(int k = 1; k <= rand; k++){
+                   if(i == userId) userInterest.add(k);
+                   interest.add(k);
                }
-               if(i >= 65){
-                   allInterest2.add(new Integer(i));
-               }
-               if(i >= 64){
-                   allInterest3.add(new Integer(i));
-               }
+               all.put(i, interest);
            }
-           all.put(new Integer(id), allInterest);
-           all.put(new Integer(2), allInterest2);
-           all.put(new Integer(44), allInterest3);
            InterestMatcher matcher = new InterestMatcher(0, 0, 0, userInterest, new ArrayList<Integer>(new Integer(2)), all);
-           Map<Integer, Double> scores = matcher.run(needed);
+           Map<Integer, Double> scores = matcher.run(range);
            //scores = matcher.sortMapByValue(scores, "asc");
-           JSONObject json = new JSONObject(matcher.run(needed));
-           String str = "";
+           JSONObject json = new JSONObject(matcher.run(range));
+
            scores.forEach((in, ins)->{
                json.put(String.valueOf(in), String.valueOf(ins));
            });
-           //System.out.println("Final string: " + str);
+           //System.out.println("
            return json.toString();
-    }
+	}
 }
 
 
--- src/main/java/com/project/team2hackathon/model/FetchInterest.java
+++ src/main/java/com/project/team2hackathon/model/FetchInterest.java
@@ -9,8 +9,73 @@ package com.project.team2hackathon.model;
  *
  * @author Oladipo Samuel
  */
+import java.sql.*;
+import java.nio.*;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+import com.project.team2hackathon.controller.InterestMatcher;
+import javax.sql.rowset.*;
+
 public class FetchInterest {
+    private int userId;
+    private String url, username, password, database;
+    private Connection con;
     
+    public FetchInterest(int userId)throws SQLException, IOException{
+        Properties props = new Properties();
+        this.userId = userId;
+        Path path = Paths.get(System.getProperty("user.dir")).resolve(("ij.properties"));
+        System.out.println(path.toString());
+        try{
+//            props.load(in);
+//            String drivers = props.getProperty("jdbc.drivers");
+//            if(drivers != null) System.setProperty("jdbc.drivers", drivers);
+//            this.url = props.getProperty("jdbc.url");
+//            this.username = props.getProperty("jdbc.username");
+//            this.password = props.getProperty("jdbc.password");
+//            this.database = props.getProperty("jdbc.database");
+//            this.con = DriverManager.getConnection(this.url, this.username, this.password); 
+            
+            
+            String drivers = "com.mysql.jdbc.Driver";
+            if(drivers != null) System.setProperty("jdbc.drivers", drivers);
+            this.url = "jdbc:mysql://40.121.9.10:3306/admin_deconnect";
+            this.username = "user_deconnect";
+            this.password = "user_deconnect123";
+            this.con = DriverManager.getConnection(this.url, this.username, this.password); 
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+    }
+    
+    public void setQuery(String query) throws SQLException, IOException{
+        Statement state = this.con.createStatement();
+        state.executeUpdate(query);
+    }
+    
+    public ResultSet getQuery(String query) throws SQLException, IOException{
+        Statement state = this.con.createStatement();
+        ResultSet result = state.executeQuery(query);
+        
+        return result;
+    }
+    
+    public InterestMatcher getMatcher(ResultSet resultUser, ResultSet resultAll)throws SQLException{
+        InterestMatcher matcher;
+        RowSetFactory reUser = RowSetProvider.newFactory();
+        CachedRowSet crs = reUser.createCachedRowSet();
+        crs.populate(resultUser); 
+        
+        crs.
+        
+        return matcher;
+    }
 }
+
+
+
 
 
